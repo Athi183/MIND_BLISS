45cbe483ceff5168798b53067edbfcfe163bdab9
FathimaMehrinVS <mehrinfiza57@gmail.com>
2025-06-26 22:24:34 +0530

    Routed Login page to Signup page

--- Mind_Bliss/src/main.jsx
+++ Mind_Bliss/src/main.jsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Login from './Login.jsx'
+import Login from './login.jsx'
 import Signup from './Signup.jsx'
 
 createRoot(document.getElementById('root')).render(
