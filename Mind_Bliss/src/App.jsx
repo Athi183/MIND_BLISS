dd2eec7690aef359cef0c3925fb5b26a59beb370
FathimaMehrinVS <mehrinfiza57@gmail.com>
2025-06-26 20:24:48 +0530

    Added Login page login.jsx and login.css

--- Mind_Bliss/src/App.jsx
+++ Mind_Bliss/src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
+import Login from './login'; // Correct import
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    
-  
-      <h1>MIND BLISS</h1>
-      
-  )
+    <div>
+      <Login /> {/* This renders your login page */}
+    </div>
+  );
 }
 
-export default App
+export default App;
